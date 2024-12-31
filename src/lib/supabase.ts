90f2d747c0988f5bfeda687ad3d3533610272645
Samarth Jain <samarthj0612@gmail.com>
2024-12-31 20:40:21 +0530

    Configured supabase and implement context provider for authentication

---
+++ src/lib/supabase.ts
@@ -0,0 +1,27 @@
+import 'react-native-url-polyfill/auto';
+import * as SecureStore from 'expo-secure-store';
+import { createClient } from '@supabase/supabase-js';
+
+const ExpoSecureStoreAdapter = {
+  getItem: (key: string) => {
+    return SecureStore.getItemAsync(key);
+  },
+  setItem: (key: string, value: string) => {
+    SecureStore.setItemAsync(key, value);
+  },
+  removeItem: (key: string) => {
+    SecureStore.deleteItemAsync(key);
+  },
+};
+
+const supabaseUrl = 'https://vbzpitndjnkxbmmjckyi.supabase.co';
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZienBpdG5kam5reGJtbWpja3lpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzU2NTIxMzYsImV4cCI6MjA1MTIyODEzNn0.OBHYF7PRleH5wMp0Xl20jpXeGJXqQIKKbxYQZcs-GtY';
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
+  auth: {
+    storage: ExpoSecureStoreAdapter as any,
+    autoRefreshToken: true,
+    persistSession: true,
+    detectSessionInUrl: false,
+  },
+});
